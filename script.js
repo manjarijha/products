21a964da1aef56609a76a0d8e6ea3e1586979fcb
manjari jha <manjari.nitdgp@gmail.com>
2021-07-04 19:06:39 +0530

    second commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+// let count = 0;
+// onclick="$('.container .callout').toggleClass('open');"
+//   console.log(count);
+// function add ()
+// {
+//     document.getElementById('btn-cart1');
+//     count++;
+//     console.log(count);
+//     document.getElementById('counter').innerHTML=count;
+
+// }
+
+let addToCartbtn = document.getElementById('addToCart');
+addToCartbtn.addEventListener('click' , function (){
+  console.log('clickedd');
+})
